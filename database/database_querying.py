875c9112b4f546a7e60800196a32c0dbb78a03c5
Sage deBrum <mightbesage@gmail.com>
2018-10-01 10:54:14 -0500

    database querying fixed

--- database/database_querying.py
+++ database/database_querying.py
@@ -8,5 +8,7 @@ username = urllib.parse.quote_plus('debrsa01')
 password = urllib.parse.quote_plus('imdaBEST65')
 client = pymongo.MongoClient("mongodb://%s:%s@cluster0-shard-00-00-mhqmc.mongodb.net:27017,cluster0-shard-00-01-mhqmc.mongodb.net:27017,cluster0-shard-00-02-mhqmc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true" % (username, password))
 
+
+db = client.test_database
 test_database = db.test_database
 pprint.pprint(test_database.find_one({"project":"senior project"}))
