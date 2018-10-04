79cccb2bd2c9fbce5b1f54a99ff104d5455981f8
Brian Ramirez <ramibr01@luther.edu>
2018-10-04 15:40:14 -0500

    Updated hello.py to print JSON object

--- flask/hello.py
+++ flask/hello.py
@@ -1,1 +1,34 @@
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+import pymongo  #document-oriented database
+import pprint  # "pretty print", allowing us a nicer format printed
+import urllib  # in coordination with an RFC
+import json
+from bson import ObjectId
 
+app = Flask(__name__)
+
+username = "debrsa01"
+password = "imdaBEST65"
+client = pymongo.MongoClient("mongodb://%s:%s@cluster0-shard-00-00-mhqmc.mongodb.net:27017,cluster0-shard-00-01-mhqmc.mongodb.net:27017,cluster0-shard-00-02-mhqmc.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true" % (username, password))
+
+
+db = client.test_database
+test_database = db.test_database
+pprint.pprint(test_database.find_one({"project":"senior project"}))
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+@app.route("/")
+def dbret():
+
+    hello_world = test_database.find_one({"project":"senior project"})
+    hello_world = JSONEncoder().encode(hello_world)
+    return hello_world
+
+if __name__ == '__main__':
+    app.run(debug = True)
