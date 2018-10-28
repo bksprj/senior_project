77898db7487ebc5994500d28966288e3fa0b4f32
Sage deBrum <mightbesage@gmail.com>
2018-10-28 13:49:51 -0500

    file uploads working in /uploader route. stored in uploads directory

--- hello.py
+++ hello.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, redirect, send_from_directory
 from flask_pymongo import PyMongo
 import pymongo  #document-oriented database
 # import urllib  # in coordination with an RFC
 import json
 from bson import ObjectId
 from werkzeug import secure_filename
+import os
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__, static_url_path='/static')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 username = "debrsa01"
 password = "imdaBEST65"
@@ -21,17 +27,54 @@ class JSONEncoder(json.JSONEncoder):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
+
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
     result_dict = test_database.find_one({"project":"senior project"})
     return render_template('index.html', result=result_dict)
 
+
+# working with uploads
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      file1 = f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
+   # if request.method == 'POST':
+   #    f = request.files['file']
+   #    file1 = f.save(secure_filename(f.filename))
+   #    return 'file uploaded successfully'
+    if request.method == 'POST':
+    # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route("/google08f628c29bd0d05f.html")
 def aftersignin():
