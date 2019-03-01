fa0a1e360d156dcfe5948269d2635ea0b501a890
husoke01 <husoke01@luther.edu>
2019-02-28 22:10:25 -0600

    Auto Update

--- static/index3.js
+++ static/index3.js
@@ -69,5 +69,5 @@ function adminOff() {
 // Changes the theme color
 function dark_mode() {
 	document.body.style.background = "#4d4d4d";
-    document.getElementsByTagName("P").style.color = "white";
+    document.getElementsById("white").style.color = "#ffffff";
 }
