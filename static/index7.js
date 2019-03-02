234f31349de3b6cf04a8fdd48bc9fdad28b59cd7
husoke01 <husoke01@luther.edu>
2019-03-01 20:18:41 -0600

    small change

--- static/index7.js
+++ static/index7.js
@@ -81,8 +81,8 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+function showModel() {
+  document.getElementById("myModal").style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -92,7 +92,7 @@ span.onclick = function() {
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 }
