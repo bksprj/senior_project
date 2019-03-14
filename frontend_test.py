a9851b025b706a113ea766ee1f29bb3495b29425
husoke01 <husoke01@luther.edu>
2019-03-14 13:39:31 -0500

    Auto Update

--- frontend_test.py
+++ frontend_test.py
@@ -1,5 +1,22 @@
 from selenium import webdriver
 import time
+import urllib.request
+import random
+
+word_url = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
+response = urllib.request.urlopen(word_url)
+long_txt = response.read().decode()
+words = long_txt.splitlines()
+
+upper_words = [word for word in words if word[0].isupper()]
+name_words  = [word for word in upper_words if not word.isupper()]
+one_name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
+
+def rand_name():
+   name = ' '.join([name_words[random.randint(0, len(name_words))] for i in range(2)])
+   return name
+
+# Start of Test
 
 # Username and Pass to sign in to gmail.
 print("Do not use with a gmail account that has two step verification.")
@@ -28,9 +45,19 @@ time.sleep(2)
 driver.find_element_by_xpath("//*[@id='passwordNext']/content/span").click()
 time.sleep(6)
 
+# Going to web app
 driver.get("https://data-sense.herokuapp.com/")
 
-mkTeam = driver.find_element_by_id("form-team")
+# Test Forming a Team
+driver.find_element_by_id("form-team").click()
+teamname = driver.find_element_by_id("group_name")
+name = rand_name()
+teamname.send_keys(name)
+admin = driver.find_element_by_id("email")
+admin.send_keys(user)
+driver.find_element_by_id("form-team-btn").click()
+
+
 
 # Stop driver
 driver.close()
